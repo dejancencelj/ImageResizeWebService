daeda62016ce0a865af2f1cbdb675eb5a5e292d4
Dejan Cencelj <dejan.cencelj@gmail.com>
2014-09-01 11:06:11 +0200

    resolve-os abanka

--- public_html/detect-redirect.js
+++ public_html/detect-redirect.js
@@ -5,11 +5,11 @@ var http = require('http'),
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   var md = new MobileDetect(request.headers['user-agent']);  
-  var location = 'http://facebook.com';
+  var location = 'http://www.abanka.si/osebne-finance/';
   if(md.os() === 'iOS'){
-      location = 'https://itunes.apple.com/us/app/facebook/id284882215?mt=8';
+      location = 'https://itunes.apple.com/app/abamobi/id845228478?ign-mpt=uo%3D5';
   }else if(md.os() === "AndroidOS"){
-     location = 'https://play.google.com/store/apps/details?id=com.facebook.katana&referrer=utm_source%3Dgoogle%26utm_medium%3Dorganic%26utm_term%3Dmarket://details?id%3Dcom.facebook.katana&pcampaignid=APPU_QyoEVI8Chcc5mKqAiAY8';
+     location = 'https://play.google.com/store/apps/details?id=co.infinum.abanka';
   }
   
     response.writeHead(302, {
